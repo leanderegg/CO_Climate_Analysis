a20696b0db836bfcf548233526cdb04c95f616ad
leanderegg <leanderegg@gmail.com>
2019-10-02 13:31:16 -0700

    Initial Repo setup and data upload

---
+++ Cd-DataCleaning.R
@@ -0,0 +1,23 @@
+
+# set working directory
+setwd("~/CO_Climate_Analysis/")
+
+
+# 2013-2014 data
+climate <- read.csv(file="TS2_13_6A_2AF15921_Fall13-June14.csv",skip=13)
+climate2 <- read.csv(file="TS3_13_81_2AE2FE21_Fall13-June14.csv",skip=13)
+climate3 <- read.csv(file="TS4_13_d2_2AFDAC21_Fall13-June14_soilT.csv",skip=13)
+
+
+
+
+plot(climate$Value, type="l")
+abline(h=0)
+lines(climate2$Value, col="blue")
+lines(climate3$Value, col="red")
+quartz()
+
+
+
+## 2014-2015 data
+
